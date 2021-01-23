a36ff0506efd6ab234dd9be1460da5dd58bcd6ea
Satyaki Bose <satyakibose98@gmail.com>
2021-01-23 15:41:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Review from "./Review";
+
+export default function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews </h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
